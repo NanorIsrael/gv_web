f07c80ec1cb9c70bbed392221c663538345eadc3
nanorisrael <israel.nanor@compozelabs.com>
2023-07-10 09:01:27 +0000

    Update projects with truehair and cimovies

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,10 @@
 import Body from '../components/Body';
-import Post from '../components/Post';
 
 function Home() {
   return (
     <Body aside>
       <article aria-label="Home">
         <section className="post_content flex flex-col items-center p-4">
-          <Post />
         </section>
       </article>
     </Body>
