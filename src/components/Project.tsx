12ae92e1b189c75d689661b743732c38ef623ff3
nanorisrael <israel.nanor@compozelabs.com>
2023-07-10 08:57:45 +0000

    Update projects with truehair and cimovies

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,29 +1,30 @@
 import { Link } from 'react-router-dom';
 
-export default function Project() {
+type ProjectProps = {
+    url: string
+    name: string
+    img: string
+    description: string
+}
+export default function Project({description, url, name, img}: ProjectProps) {
   return (
     <div className="card p-4 bg-white rounded overflow-hidden shadow-md w-100 h-80 m-2 lg:w-64 m-1">
       <figure>
-        <img src="/images/projects/rw.png" alt="project-rw" />
+        <img src={`/images/projects/${img}.png`} alt="project-rw" />
         <figcaption>
           <p>
             <span className="">
               <strong>Name:</strong>
             </span>
-            <Link target={'_blank'} to="https://foodscrapspickup.com">
-              Ramsey/Washington
+            <Link target={'_blank'} to={url}>
+             {name}
             </Link>
           </p>
           <p>
             <span>
               <strong>Description:</strong>
             </span>
-            For residents of Washington & Ramsey County who would like to
-            subscribe to the new food scraps pickup program and want to learn
-            more about the program, the R&W food scraps pickup program website
-            will allow users to order their free annual supply of food scrap
-            bags yearly or order additional bags at a cost and to learn more
-            about the program.
+            {description}
           </p>
         </figcaption>
       </figure>
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -2,13 +2,45 @@ import Body from '../components/Body';
 import Project from '../components/Project';
 
 function Projects() {
+  const project = [
+    { 
+    id: 1,
+    url: "https://foodscrapspickup.com",
+    name: "Ramsey/Washington", 
+    description:"For residents of Washington & Ramsey County who would like to \
+    subscribe to the new food scraps pickup program and want to learn \
+    more about the program, the R&W food scraps pickup program website \
+    will allow users to order their free annual supply of food scrap \
+    bags yearly or order additional bags at a cost and to learn more \
+    about the program.",
+    img: "rw",
+  },
+  {
+    id: 2,
+    url: "https://nanorisrael.github.io/movie_store/",
+    name:'cimoviez', 
+    description:`${"A movie library will all information you need about a movie. A personal project and still in progress."}`,
+    img: "cimoviez",
+  },
+  {
+    id: 3,
+    url: "https://animated-sopapillas-60dde9.netlify.app/book-now",
+    name:'TruHair & Beaty', 
+    description:`${"A booking application to enable a beautician client, serve her customers \
+     with all their beauty needs and provide other online services. A personal project and still in progress."}`,
+    img: "truhair",
+  }
+]
   return (
     <Body aside>
       <article aria-label="Projects">
-        <Project />
-        <Project />
-        <Project />
-      </article>
+       { project.map(p => <Project
+        description={p.description}
+        url={p.url} 
+        name={p.name}
+        img={p.img}
+        key={p.id}/>)}
+       </article>
     </Body>
   );
 }
